2d5038bb822ddae3347775e6e882ad420f428de2
Pipeperez1305 <af.perezm1@uniandes.edu.co>
2025-03-19 18:04:47 -0500

    changes

---
+++ lib/views/myevents_view.dart
@@ -0,0 +1,206 @@
+import 'package:dart_g21/controllers/event_controller.dart';
+import 'package:dart_g21/controllers/profile_controller.dart';
+import 'package:dart_g21/controllers/user_controller.dart';
+import 'package:dart_g21/models/event.dart';
+import 'package:flutter/material.dart';
+import 'package:dart_g21/core/colors.dart';
+import 'package:dart_g21/widgets/navigation_bar_host.dart';
+
+class MyEventsPage extends StatefulWidget {
+  final String userId;
+
+  MyEventsPage({Key? key, required this.userId}) : super(key: key);
+
+  @override
+  _MyEventsPageState createState() => _MyEventsPageState();
+}
+
+class _MyEventsPageState extends State<MyEventsPage> {
+  ProfileController _profileController = ProfileController();
+  EventController _eventController = EventController();
+  UserController _userController = UserController();
+  int selectedIndex = 2; // Índice del ícono seleccionado (My Events)
+  List<Event> upcomingEvents = [];
+  List<Event> previousEvents = [];
+  String profileId = "";
+
+  Future<void> _loadProfileAndEvents() async {
+    try {
+      final profile = await _profileController.getProfileByUserId(widget.userId).first;
+      if (profile != null) {
+        List<Event> events = await _eventController.getEventsByIds(profile.events_associated);
+        List<List<Event>> classifiedEvents = _eventController.classifyEvents(events);
+        setState(() {
+          upcomingEvents = classifiedEvents[0];
+          previousEvents = classifiedEvents[1];
+          profileId = profile.id;
+        });
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Text("My Events", style: TextStyle(fontSize: 24, color: AppColors.textPrimary)),
+          ],
+        ),
+      ),
+
+      // Cuerpo con lista de eventos
+      body: FutureBuilder(
+        future: _loadProfileAndEvents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              children: [
+                //  Sección de Upcoming Events
+                buildSectionTitle("Upcoming Events"),
+                ...upcomingEvents.map((event) => buildEventCard(event, profileId)).toList(),
+
+                SizedBox(height: 20),
+
+                //  Sección de Previous Events
+                buildSectionTitle("Previous Events"),
+                ...previousEvents.map((event) => buildEventCard(event, profileId)).toList(),
+              ],
+            );
+          }
+        },
+      ),
+
+      //  Barra de navegación inferior
+      // bottomNavigationBar: BottomNavBarHost(
+      //   selectedIndex: selectedIndex,
+      //   onItemTapped: (index) {
+      //     setState(() {
+      //       selectedIndex = index;
+      //     });
+      //   },
+      // ),
+    );
+  }
+
+  // Widget para los títulos de sección ("Upcoming Events", "Previous Events")
+  Widget buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  //  Widget para cada tarjeta de evento
+  Widget buildEventCard(Event event, String profileId) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          print("Evento seleccionado: ${event.name}");
+        },
+        borderRadius: BorderRadius.circular(12),
+        splashColor: Colors.blue.withOpacity(0.2),
+        highlightColor: Colors.blue.withOpacity(0.1),
+        child: Card(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          elevation: 4,
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //  Imagen y pago
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.network(
+                          event.image,
+                          width: 120,
+                          height: 120,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      if (event.cost != 0)
+                        Icon(Icons.payments, color: AppColors.textPrimary),
+                    ],
+                  ),
+
+                  SizedBox(width: 12),
+
+                  //  Detalles del evento alineados arriba
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Text(
+                          event.start_date.toString(),
+                          style: TextStyle(fontSize: 12, color: AppColors.secondary),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          event.name,
+                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //  Botones de guardar y eliminar bien alineados
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.flag_outlined, color: AppColors.textPrimary),
+                        onPressed: () {},
+                      ),
+                      Spacer(),
+                      IconButton(
+                        icon: Icon(Icons.delete_outline, color: AppColors.textPrimary),
+                        onPressed: () {
+                          print("Eliminar evento: ${event.name}");
+
+                          //  Eliminar evento de la lista de eventos asociados del perfil
+                          _profileController.removeEventFromProfile(profileId, event.id);
+
+                          //  Actualizar la lista de eventos
+                          _loadProfileAndEvents();
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("Event removed from My Events"),
+                              duration: Duration(seconds: 2),
+                            ),
+                          );
+
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
